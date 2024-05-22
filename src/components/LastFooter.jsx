dc2f4e9a2f307e993da0b7f0d4b2864eef2c2fc9
RohithStalin <stalinrohith219@gmail.com>
2024-05-22 11:12:15 +0530

    added lastfooter

---
+++ src/components/LastFooter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const LastFooter = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+
+        marginTop: "10vh",
+        padding: "0 10%",
+      }}
+    >
+      <div>
+        <ul style={{ listStyleType: "none" }} className="lastFooter">
+          <li>Top Uk universities</li>
+          <li>MBA in UK</li>
+          <li>MBA International Buisiness</li>
+        </ul>
+      </div>
+      <div>
+        <ul style={{ listStyleType: "none" }} className="lastFooter">
+          <li>Top Uk universities</li>
+          <li>MBA in UK</li>
+          <li>MBA International Buisiness</li>
+        </ul>
+      </div>
+      <div>
+        <ul style={{ listStyleType: "none" }} className="lastFooter">
+          <li>Top Uk universities</li>
+          <li>MBA in UK</li>
+          <li>MBA International Buisiness</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LastFooter;
