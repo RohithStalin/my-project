ab6fa9c4501dca23f0f55d749aae5573aacfb785
RohithStalin <stalinrohith219@gmail.com>
2024-05-22 11:04:40 +0530

    Added App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MapPractice from "./components/MapPractice";
+// import SignUp from "./form/SignUp";
+function App() {
+  return (
+    <div>
+      <div>
+        {/* <SignUp /> */}
+        <MapPractice />
+      </div>
+    </div>
+  );
+}
+
+export default App;
