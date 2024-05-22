1c0100beee8ac912fb263f736457f117355fee1f
RohithStalin <stalinrohith219@gmail.com>
2024-05-22 11:06:22 +0530

    Added footer.jsx file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "40vh",
+          padding: "0 14%",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">Study Abroad</h4>
+            </li>
+            <li>Study in UK</li>
+            <li>Study in Ireland</li>
+            <li>Study in USA</li>
+            <li>Study in Canada</li>
+            <li>Study in Germany</li>
+            <li>Study in Dubai</li>
+            <li>Study in Australia</li>
+            <li>Study in New Zealand</li>
+          </ul>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">Coaching</h4>
+            </li>
+            <li>IELTS</li>
+            <li>GMAT</li>
+            <li>GRE</li>
+            <li>SAT</li>
+            <li>TOEFL</li>
+            <li>PTE</li>
+          </ul>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">Other Services</h4>
+            </li>
+            <li>Accomodation</li>
+            <li>Forex</li>
+            <li>Flights</li>
+          </ul>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">About AnyUNI</h4>
+            </li>
+            <li>Contact Us</li>
+            <li>Careers</li>
+            <li>Privacy</li>
+            <li>Terms & Conditions</li>
+            <li>Refund Policy</li>
+          </ul>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "10vh",
+          padding: "0 14%",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="image">
+          <svg
+            width="253"
+            height="103"
+            viewBox="0 0 253 103"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="0.333984"
+              y="0.334961"
+              width="252.666"
+              height="102.33"
+              rx="5"
+              fill="white"
+            />
+            <g clip-path="url(#clip0_1_2381)">
+              <path
+                d="M59.3402 65.1544H48.3585L45.8006 58.2568H31.5903L28.8648 65.1544H18.0195L33.2331 25.6016H44.4506L59.3402 65.1544ZM43.0296 50.4354L38.7665 37.8224L34.4495 50.4354H43.0296Z"
+                fill="#001B39"
+              />
+              <path
+                d="M91.3587 65.155H81.8265V51.5558C81.8265 49.1836 81.5574 47.6119 81.0193 46.8408C80.233 45.704 79.0526 45.1356 77.4781 45.1356C76.8049 45.1192 76.1354 45.2411 75.5111 45.4938C74.8868 45.7466 74.3211 46.1247 73.8488 46.6049C72.9659 47.4897 72.5244 48.8738 72.5244 50.7571V65.1578H62.9922V38.2635H72.5244V41.654C73.6253 40.3808 74.638 39.4827 75.5626 38.9598C77.0651 38.1109 78.9456 37.6865 81.2041 37.6865C84.0973 37.6865 86.4505 38.4681 88.2637 40.0312C90.3176 41.8103 91.3493 44.4762 91.3587 48.0288V65.155Z"
+                fill="#001B39"
+              />
+              <path
+                d="M130.711 38.2629L109.111 78.6002H98.4508L107.579 61.4739L94.6055 38.2629H105.644L112.832 52.0582L119.814 38.2629H130.711Z"
+                fill="#001B39"
+              />
+              <path
+                d="M169.362 48.4062C169.397 50.4492 169.207 52.4899 168.794 54.4911C168.195 57.1001 166.765 59.476 164.505 61.6189C161.297 64.6505 157.008 66.1662 151.639 66.1662C146.171 66.1662 141.901 64.6211 138.83 61.5308C137.171 59.8423 136.018 57.7239 135.499 55.4147C135.073 53.6792 134.859 51.3421 134.859 48.4034V25.6016H145.148V46.9966C145.148 48.7132 145.177 49.9959 145.236 50.8447C145.425 53.5314 146.651 55.4034 148.913 56.4606C149.874 56.9132 150.924 57.1416 151.986 57.1285C154.498 57.1285 156.393 56.2503 157.67 54.4939C158.617 53.2017 159.091 50.7026 159.091 46.9966V25.6016H169.376L169.362 48.4062Z"
+                fill="#FE0000"
+              />
+              <path
+                d="M217.144 65.1544H206.859L187.937 40.9288V65.1544H177.648V25.6016H187.937L206.859 49.796V25.6016H217.144V65.1544Z"
+                fill="#FE0000"
+              />
+              <path
+                d="M235.722 65.1544H225.434V25.6016H235.722V65.1544Z"
+                fill="#FE0000"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1_2381">
+                <rect
+                  x="18.0195"
+                  y="22.9688"
+                  width="223.056"
+                  height="55.6315"
+                  rx="5"
+                  fill="white"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">Global Office</h4>
+            </li>
+            <li>86-90 Paul Street </li>
+            <li>London EC2M 4NE</li>
+            <li>United Kingdom</li>
+          </ul>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }} className="items">
+            <li>
+              <h4 className="footerHeading">India Head Office</h4>
+            </li>
+            <li>123 Street Table Tennis </li>
+            <li>Badminton</li>
+            <li>India</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
